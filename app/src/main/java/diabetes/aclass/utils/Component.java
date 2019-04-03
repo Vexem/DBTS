eb96b4162dfb65a436c28c650e6890f8a4a31267
paolopmb87 <paolo.pmb@gmail.com>
2019-04-03 13:03:44 +0200

    no message

--- app/src/main/java/diabetes/aclass/utils/Component.java
+++ app/src/main/java/diabetes/aclass/utils/Component.java
@@ -3,7 +3,7 @@ package diabetes.aclass.utils;
 public class Component {
 //    public static final String API_BASE = "http://192.168.1.73/api/v1/";
 //    public static final String API_BASE = "http://192.168.43.70/";
-        public static final String API_BASE = "http://192.168.1.7/";
+    public static final String API_BASE = "http://192.168.1.7/";
     public static final String API_GET_USERS = API_BASE + "users";
     public static final String API_GET_USER_BYID = API_BASE + "users/getuserbyid?user_id=";
     public static final String API_POST_USER = API_BASE + "users/saveuser";
@@ -13,6 +13,7 @@ public class Component {
     public static final String API_GET_MEASUREMENTS = API_BASE + "measurements";
     public static final String API_POST_MEASUREMENTS = API_BASE + "measurements/save";
     public static final String API_GET_MEASUREMENTS_BYID = API_BASE + "measurements/getbyuid?patient_id=";
+    public static final String API_GET_LASTMONTH_MEASUREMENT_BYID = API_BASE + "measurements/patientmeasord?patient_id=";
 
     public static final String GOOGLE_AUTH = "http://dbts.com/auth/google_oauth2/";
 }
